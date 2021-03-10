65eedc1803ddc85cdc35e092f792e94a0fc29e80
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-03-10 16:46:48 +0700

    Add files via upload

---
+++ mtk.sh
@@ -0,0 +1,94 @@
+sleep 1
+echo -e "\e[93mhalo bro :)\e[0m";
+sleep 1
+echo -e "\e[94mhecked by M3R4Z\e[0m";
+sleep 1
+echo -e "\e[93mjawab pertanyaan yang ada?\e[0m";
+sleep 1
+echo -e "\e[94msoal mtk\e[0m";
+sleep 1
+echo -e "\e[91mmasukan nama Anda\e[0m";
+read -p '$>' userinput1;
+#
+sleep 1
+echo -e "\e[92mjawab pertanyaan berikut!\e[0m";
+sleep 1
+echo -e "\e[92m5+5+5+5-10+10= ?\e[0m";
+read -p '$>' userinput2;
+#
+if [ "${userinput2:-}" = "20" ]
+then
+	echo -e "\e[94mbenar\e[0m";
+fi
+if [ "${userinput2:-}" != "20" ]
+then
+	echo -e "\e[94msalah\e[0m";
+fi
+sleep 1
+echo -e "\e[96m5+10+50-60×5=?\e[0m";
+read -p '$>' userinput3;
+#
+if [ "${userinput3:-}" = "25" ]
+then
+	echo -e "benar";
+fi
+if [ "${userinput3:-}" != "25" ]
+then
+	echo -e "\e[91msalah\e[0m";
+fi
+sleep 1
+echo -e "\e[94m50+50+50-50=?\e[0m";
+read -p '$>' userinput4;
+#
+if [ "${userinput4:-}" = "100" ]
+then
+	echo -e "\e[93mbenar\e[0m";
+fi
+if [ "${userinput4:-}" != "100" ]
+then
+	echo -e "\e[91msalah\e[0m";
+fi
+sleep 1
+echo -e "\e[94m5×5+10+50-60=?\e[0m";
+read -p '$>' userinput5;
+#
+if [ "${userinput5:-}" = "25" ]
+then
+	echo -e "\e[92mbenar\e[0m";
+fi
+if [ "${userinput5:-}" != "25" ]
+then
+	echo -e "\e[93msalah\e[0m";
+fi
+sleep 1
+echo -e "\e[94m0×50=?\e[0m";
+sleep 0.5
+read -p '$>' userinput5;
+sleep 0.5
+if [ "${userinput5:-}" = "0" ]
+then
+	echo -e "\e[91mbenar\e[0m";
+fi
+if [ "${userinput5:-}" != "0" ]
+then
+	echo -e "\e[94msalah\e[0m";
+fi
+sleep 1
+echo -e "\e[92mauthor sc lagi gabut :V\e[0m";
+sleep 1
+echo -e "\e[92msubrek chennel MR M3R4Z\e[0m";
+sleep 1
+echo -e "\e[94mketik exit untuk keluar\e[0m";
+sleep 0.6
+read -p '$>' userinput5;
+sleep 2
+echo -e "\e[92msabar ya :)\e[0m";
+sleep 5
+echo -e "\e[92msabar ya :)\e[0m";
+sleep 4
+echo -e "\e[92msabar ya :)\e[0m";
+sleep 5
+echo -e "\e[92msabar ya :)\e[0m";
+sleep 2
+echo -e "\e[92hihihihi :')\e[0m";
+sleep 5
